4d86e55358f3fec4ff742bc9d31b1cf22642dc9e
Tereza Stoyanova <stoyanova.tereza@gmail.com>
2018-12-18 17:23:18 -0500

    Sum up the integers within a given range

---
+++ SumRange/SumTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class SumTest {
+    
+    Sum s = new Sum();
+
+    @Test
+    public void Test()
+    {
+      assertEquals(-1, s.GetSum(0, -1));
+      assertEquals(1, s.GetSum(0, 1));
+    }
+
+}
